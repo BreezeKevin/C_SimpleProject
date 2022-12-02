27da61cf347508d431d7c89b3d89f2ef7b08a558
KevinLee <lk3437456@163.com>
2022-12-03 01:37:41 +0800

    忽略build下的所有文件,保留build目录

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Files/c++/mycode
-# Build directory: D:/Files/c++/mycode/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
